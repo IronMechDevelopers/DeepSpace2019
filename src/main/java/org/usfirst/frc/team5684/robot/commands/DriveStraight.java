80878985e2e7de1efbf848c9c7705caea354a26e
IronMechsRobotics <ironmechs@trentoncatholic.org>
2019-03-19 17:25:03 -0400

    turn off the motors when driveStraight is done

--- src/main/java/org/usfirst/frc/team5684/robot/commands/DriveStraight.java
+++ src/main/java/org/usfirst/frc/team5684/robot/commands/DriveStraight.java
@@ -49,11 +49,13 @@ public class DriveStraight extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.driveTrain.DriveStraight(0, 0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    this.end();
   }
 }
